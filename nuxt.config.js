27aff9de63cb23bda4c89cdf1c0cf474f2ffd18e
Ezra <ezraabate8@gmail.com>
2022-03-01 10:07:10 +0300

    test commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,6 +26,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '~/plugins/barcode', ssr: false },
+
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
--- pages/test.vue
+++ pages/test.vue
@@ -1,3 +1,31 @@
 <template>
-  <input type="file" accept="image/*;capture=camera" />
+  <div>
+    <input type="file" accept="image/*;capture=camera" />
+    <div>
+      <v-quagga
+        :onDetected="logIt"
+        :readerSize="readerSize"
+        :readerTypes="['ean_reader']"
+      ></v-quagga>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      readerSize: {
+        width: 640,
+        height: 480,
+      },
+      detecteds: [],
+    };
+  },
+  methods: {
+    logIt(data) {
+      console.log("detected", data);
+    },
+  },
+};
+</script>
