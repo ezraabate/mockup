43cbbaebd005b475600fc2b2cb8429adb018d1e3
Ezra <ezraabate8@gmail.com>
2022-03-03 09:24:43 +0300

    test

--- pages/test.vue
+++ pages/test.vue
@@ -1,19 +1,41 @@
 <template>
   <div>
-    <input type="file" accept="image/*;capture=camera" />
+    <input
+      @change="imageFile($event)"
+      type="file"
+      accept="image/*;capture=camera"
+    />
     <h1>Updated</h1>
+
     <div>
-      <v-quagga
+      <img :src="imageData" alt="" style="width: 100px; height: 100px" />
+      <!-- <v-quagga
         :onDetected="logIt"
         :readerSize="readerSize"
         :readerTypes="['ean_reader']"
-      ></v-quagga>
+      ></v-quagga> -->
+      <img />
     </div>
   </div>
 </template>
 
 <script>
+// const Client = require("@veryfi/veryfi-sdk");
+// import Client from "@veryfi/veryfi-sdk";
 export default {
+  // async created() {
+  //   const client_id = "vrfdBR7u5cjwOvAXtsacPf0WJGiLXgZT0zTLg4M";
+  //   const client_secret =
+  //     "C3EYeAXPHzN1fb7klo1peBipo0ine8VwYug6I5s7i55g5Rsxrgn4GWos0UZljDeNBBJ7L8CohBWcYjmzRTsrWTBCeiJheOHEUoeElJ0vjYPG7G77Xc19XZfs2YErdyGB";
+  //   const username = "ezra1";
+  //   const api_key = "c7fa47314ac6af0207b602c311e90c68";
+
+  //   let my_client = new Client(client_id, client_secret, username, api_key);
+
+  //   const response = async () => await my_client.process_document("two.jpg");
+
+  //   response().then(console.log);
+  // },
   data() {
     return {
       readerSize: {
@@ -21,11 +43,17 @@ export default {
         height: 480,
       },
       detecteds: [],
+      imageData:
+        "https://static.scientificamerican.com/sciam/cache/file/4F73FD83-3377-42FC-915AD56BD66159FE_source.jpg",
     };
   },
   methods: {
     logIt(data) {
       console.log("detected", data);
+    },
+    imageFile(event) {
+      console.log("event", event.target.files[0]);
+      this.imageData = URL.createObjectURL(event.target.files[0]);
     },
   },
 };
